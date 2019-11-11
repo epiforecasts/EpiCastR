ce3bfd8c6ca3fd06bfbe3ba4336a6b896e24df9b
James <Munday>
2019-11-11 15:48:32 +0000

    initial commit

---
+++ R/fit_model.r
@@ -0,0 +1,71 @@
+
+
+
+require(rstan)
+
+
+source('../R_HA/prepare_parameters.r')
+source('../R_HA/construct_stan_model.r')
+
+fit_model <- function(timeseries, shapes, timestep=1, period_and_lag=c(5,7), identifier="ADM2_NAME", 
+                      popid='totpop2019', interaction=c(1), distrib=0, model_path='../stan/template.stan', fit_meth = 'vb') {
+  
+  params = prepare_stan_inputs(timeseries, shapes, timestep, period_and_lag, identifier, 
+                                 popid, interaction, distrib)
+  
+  datalist = params[[1]]
+  ordered_shapes = params[[2]]
+  
+  if (distrib == 1){
+    
+    interactions = append(interaction, c(5))
+    
+  } else { 
+    
+    interactions = interaction
+    
+    }
+  
+  model = construct_stan_model(model_path, interactions)
+  
+  write(model, "../R_HA/model_running.stan")
+  
+  pars = c()
+  
+  if (1 %in% interactions){ 
+    pars = append(pars, c("k", "gamma", "alpha_spat"))
+  }
+  
+  if (2 %in% interactions){ 
+    pars = append(pars, c("k", "gamma", "alpha_spat"))
+  }
+  
+  if (3 %in% interactions){ 
+    pars = append(pars, c("k", "gamma", "alpha_spat"))
+  }
+  
+  if (4 %in% interactions){ 
+    pars = append(pars, c("gamma", "alpha_adj"))
+  }
+  
+  if (5 %in% interactions){ 
+    pars = append(pars, c("beta"))
+  }
+  
+  pars = unique(pars)
+  
+  
+  sm = stan_model(model_code = model)
+  
+  if (fit_meth == 'nuts'){
+    fit1 <- sampling(sm, data = datalist, chains = chains, iter = iter, warmup = warmup, cores = cores,pars =pars)    # fit stan model
+  }
+  if (fit_meth == 'vb'){
+    fit1 <- vb(sm, data = datalist, pars = pars)    # fit stan model
+  }
+  
+  list(fit = fit1, data = datalist, ordered_shapes)
+    
+}
+  
+  
---
+++ R/prepare_parameters.r
@@ -0,0 +1,87 @@
+library(rstan)
+library(sf)
+library(dplyr)
+library(zoo)
+library(spdep)
+
+prepare_stan_inputs <- function(timeseries, shapes, timestep=1, period_and_lag=c(5,7), identifier="ADM2_NAME", 
+                                popid='totpop2019', interaction=c(1), distrib=0){
+  
+  
+  
+  
+  R = dim(timeseries)[1]                                                   # Set number of areas R
+  Ti = dim(timeseries)[2]/timestep                                         # Set nuber of days
+  
+  shapes_ordered = shapes[order(shapes[[identifier]]),]                    # set order of areas to alphabetical to ensure consistancy 
+  
+  shapes_ordered['AREA'] = st_area(shapes_ordered)
+  
+  sf_cent = st_centroid(shapes_ordered)  # find centroids of the polygons
+
+  distmat = st_distance(sf_cent)                                           # matrix of distance between centroids as 1D matrix for denom of grav model
+  distmat = matrix(distmat, ncol=R, nrow=R)                                # format matrix to correct shape
+  distmat[distmat < 1.] = 1.                                               # set minimum disance to 1. (avoiding devide by 0 (over written in stan for final interaction mat))
+  
+  
+  
+  
+  
+  
+  
+  # SET GRAVITY INTERACTION
+  
+        
+        if (1 %in% interaction){
+          popmat = outer(as.vector(shapes_ordered[[popid]]), as.vector(shapes_ordered[[popid]]))      # set population matrix for numerator of grav model
+          popmat[is.na(popmat)] = 1.
+        } else if (2 %in% interaction){
+          popmat = outer(as.vector(shapes_ordered[[popid]] / shapes_ordered[["AREA"]]), as.vector(shapes_ordered[[popid]]/shapes_ordered[["AREA"]]))      # set population matrix for numerator of grav model
+          popmat[is.na(popmat)] = 1.
+        } else if (3 %in% interaction){
+          popmat = matrix(1, ncol=R, nrow=R)      # set population matrix for numerator of grav model
+          popmat[is.na(popmat)] = 1.
+        } else{ 
+          popmat = matrix(0., ncol=R, nrow=R)      # set population matrix for numerator of grav model
+        }
+  
+  # SET ADJACENCY INTERACTION
+  
+        if (4 %in% interaction){
+          nbsdrc = poly2nb(DRC_ordered)
+          adjmat = nb2mat(nbsdrc, zero.policy=TRUE)
+        } else {
+          adjmat = matrix(0., ncol=R, nrow=R) 
+        }
+  
+  
+  if (timestep != 1){
+    cases_to = t(rollapply(t(timeseries), timestep, sum, by=timestep))
+  } else {
+    cases_to = timeseries
+  }
+  
+  rolledsums = t(rollapply(t(cases_to), period_and_lag[1], sum))
+  
+  summed_cases_offsett = cbind(matrix(0, nrow=R, ncol=sum(period_and_lag)), rolledsums)
+  summed_cases_offsett = summed_cases_offsett[,1:Ti]
+        
+  cases_from = summed_cases_offsett
+  
+  
+  datalist = list(
+    
+    R = R,
+    T = Ti,
+    MIJ = distmat/1000.0,
+    popmat = popmat,
+    adjmat = adjmat,
+    N = cases_to,
+    Nsum = cases_from,
+    distrib = distrib
+           )
+  
+  list(datalist, shapes_ordered)
+  
+  
+}
